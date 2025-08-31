47d32f291aaa811061b491a45efb7c3c5c5eca3e
Yuvraj Rajni Sachin Deshmukh <yuvraj280605@gmail.com>
2025-08-31 12:37:53 +0530

    Phase 1(b)

--- init/index.js
+++ init/index.js
@@ -3,6 +3,7 @@ const initData=require("./data.js");
 const Listing=require("../models/listing.js");
 const MONGO_URL="mongodb://127.0.0.1:27017/wanderlust";
 
+
 main().then(()=>{
     console.log("MongoDB connected");
 })
