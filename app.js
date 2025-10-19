39432b7b11ebe24546dadd54b35bad9a9c1c85b5
Yuvraj Rajni Sachin Deshmukh <yuvraj280605@gmail.com>
2025-10-19 08:44:49 +0530

    Phase 1(c)

--- app.js
+++ app.js
@@ -6,7 +6,8 @@ const path=require("path");
 const { title } = require("process");
 const methodOverride=require("method-override");
 const ejsMate=require("ejs-mate");
-const wrapSync=require("/utils/wrapAsync.js");
+const wrapAsync=require("./utils/wrapAsync.js");
+const ExpressError=require("./utils/ExpressError.js");
 
 const MONGO_URL="mongodb://127.0.0.1:27017/wanderlust";
 
@@ -32,10 +33,11 @@ app.get("/",(req,res)=>{
     res.send("Hi,I am root");
 });
 //Index Route
-app.get("/listings",async (req,res)=>{
+app.get("/listings",wrapAsync(async (req,res)=>{
     const allListings = await Listing.find({});
     res.render("./listings/index.ejs",{allListings});
-});
+}));
+
 
 //New Route
 app.get("/listings/new",(req,res)=>{
@@ -43,11 +45,11 @@ app.get("/listings/new",(req,res)=>{
 });
 
 //Show Route
-app.get("/listings/:id",async (req,res)=>{
+app.get("/listings/:id",wrapAsync(async (req,res)=>{
     let {id}=req.params;
     const listing = await Listing.findById(id);
     res.render("listings/show.ejs",{listing});
-});
+}));
 
 function normalizeImageUrl(listingData) {
   if (listingData.image && listingData.image.url === "") {
@@ -66,29 +68,36 @@ app.post("/listings",wrapAsync(async (req,res,next)=>{
 );
 
 //Edit Route
-app.get("/listings/:id/edit",async (req,res)=>{
+app.get("/listings/:id/edit",wrapAsync(async (req,res)=>{
     let { id }=req.params;
     const listing = await Listing.findById(id);
     res.render("listings/edit.ejs",{ listing });
-});
+}));
 
 //Update Route
-app.put("/listings/:id",async (req,res)=>{
+app.put("/listings/:id",wrapAsync(async (req,res)=>{
     let { id }=req.params;
     await Listing.findByIdAndUpdate(id,{...req.body.listing});
     res.redirect(`/listings/${id}`);
-});
+}));
 
 //Delete Route
-app.delete("/listings/:id",async (req,res)=>{
+app.delete("/listings/:id",wrapAsync(async (req,res)=>{
     let { id }=req.params;
     let deleted = await Listing.findByIdAndDelete(id);
     console.log(deleted);
     res.redirect("/listings");
-});
+}));
+
+app.use((req,res,next)=>{
+    next(new ExpressError(404,"Page Not Found!"));
+})
+
 
 app.use((err,req,res,next)=>{
-    res.send("Something went wrong!");
+    let{statusCode=500,message="Something went wrong!"}=err;
+    res.render("error.ejs",{message});
+    // res.status(statusCode).send(message);
 })
 
 app.listen(8080,()=>{
